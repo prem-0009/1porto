3af51f234dfd07897d2ecfb81f02d436800e5ed8
prem <bleu.namka@gmail.com>
2021-10-07 18:00:37 -0400

    footer email n animation added

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -2,6 +2,10 @@ import React, { useState, useEffect } from "react";
 import "./home.css";
 import { Container } from "@mui/material";
 import project1 from "../../images/project1.png";
+import ContentCopyIcon from "@mui/icons-material/ContentCopy";
+import Badge from '@mui/material/Badge';
+import MailIcon from '@mui/icons-material/Mail';
+
 
 const Home = () => {
   const today = new Date();
@@ -26,7 +30,7 @@ const Home = () => {
 
   const Works = () => {
     return (
-      <div className='work-main'>
+      <div className="work-main">
         <div className="work">
           {/* change the github link to actual movie site */}
           <a
@@ -36,12 +40,40 @@ const Home = () => {
             <span>search movies</span>
           </a>
         </div>
-        <a className="link-github" href="https://github.com/prem-0009/Pmovies">
+        <a
+          className="link-github"
+          href="https://github.com/prem-0009/Pmovies"
+          target="_blank"
+        >
           Github
         </a>
       </div>
     );
   };
+
+  //   const [flipContact, setFlipContact] = useState("email");
+  const [copyText, setCopyText] = useState("email");
+  const [copySuccess, setCopySuccess] = useState("");
+
+  const handleFlip = (e) => {
+    setCopyText("prem.sherpa890@gmail.com");
+
+    setTimeout(() => {
+      setCopyText("email");
+    }, 6000);
+  };
+
+  const handleIconClick = (e) => {
+    setCopyText("prem.sherpa890@gmail.com");
+
+    document.execCommand(copyText);
+    navigator.clipboard.writeText(copyText);
+    setCopySuccess("copied");
+    setTimeout(() => {
+        setCopySuccess('')
+    }, 500);
+  };
+
 
   useEffect(() => {
     greeting();
@@ -49,7 +81,6 @@ const Home = () => {
 
   return (
     <div className="home-body">
-      {/* <h1 className="header"></h1> */}
       <Container>
         <h2 className="home-header">Good {timeNow}</h2>
         <h3 className="work-title">My Project</h3>
@@ -57,6 +88,31 @@ const Home = () => {
         {/* <a className="link-github" href="https://github.com/prem-0009/Pmovies">
           Github
         </a> */}
+        {/* <span className='home-footer'>contact</span> */}
+        {/* <Contact /> */}
+        <div
+          className="home-footer"
+          onClick={(e) => handleFlip(e)}
+          //   value={copyText}
+        >
+          {copyText === "email" ? (
+            copyText
+          ) : (
+            <div className="email-footer">
+              <span>{copyText}</span>&nbsp;&nbsp;
+              <Badge badgeContent={copySuccess} 
+              sx={{padding:'1px' }}
+            //   color=''
+              >
+                <ContentCopyIcon
+                  fontSize="small"
+                  onClick={(e) => handleIconClick(e)}
+                  />
+                  </Badge>
+            </div>
+          )}
+          {/* {copySuccess} */}
+        </div>
       </Container>
     </div>
   );
